deb540492db85a7961a8df09d1399c2f2e348121
M4rqu1705 <m4rc05.dev@gmail.com>
2020-08-26 00:03:12 -0400

    Updates

---
+++ precache-manifest.5aecab3acdfac8336cca9720bacf3397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d714bc0378fdcc2eda48e19c55c63375",
+    "url": "/SWOT-Web-App/index.html"
+  },
+  {
+    "revision": "514c503d1f36f03a863e",
+    "url": "/SWOT-Web-App/static/css/main.7cf4b2b2.chunk.css"
+  },
+  {
+    "revision": "6ce5373b75dca1cdec25",
+    "url": "/SWOT-Web-App/static/js/2.43226a1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SWOT-Web-App/static/js/2.43226a1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514c503d1f36f03a863e",
+    "url": "/SWOT-Web-App/static/js/main.292841d4.chunk.js"
+  },
+  {
+    "revision": "17b98ed68b4151ff5454",
+    "url": "/SWOT-Web-App/static/js/runtime-main.037871fc.js"
+  }
+]);
